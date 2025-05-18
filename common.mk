66d6e81e4f7c8f5e96258cfb5ba08ec2b614fca7
流光 <i@dreamvoid.me>
2025-05-18 11:13:05 +0800

    sdm845-common: 添加 qcom-caf/bootctrl 配置

--- common.mk
+++ common.mk
@@ -325,6 +325,7 @@ PRODUCT_SOONG_NAMESPACES += \
     hardware/google/pixel \
     hardware/lineage/interfaces/power-libperfmgr \
     hardware/oneplus \
+    hardware/qcom-caf/bootctrl \
     vendor/qcom/opensource/usb/etc
 
 # Telephony
