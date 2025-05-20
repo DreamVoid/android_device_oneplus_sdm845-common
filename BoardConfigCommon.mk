2b9fc6e5add1bacb4c628d41d649b9ca673758c4
流光 <i@dreamvoid.me>
2025-05-20 16:05:46 +0800

    修改sepolicy路径

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -176,7 +176,7 @@ VENDOR_SECURITY_PATCH := 2021-11-01
 
 # Sepolicy
 include device/lineage/sepolicy/libperfmgr/sepolicy.mk
-include device/qcom/sepolicy_vndr-legacy-um/SEPolicy.mk
+include device/qcom/sepolicy_vndr/legacy-um/SEPolicy.mk
 
 BOARD_VENDOR_SEPOLICY_DIRS += $(COMMON_PATH)/sepolicy/vendor
 PRODUCT_PUBLIC_SEPOLICY_DIRS += $(COMMON_PATH)/sepolicy/public
